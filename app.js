8c4e20cfb87790849c58a62d6b46861cbb863c47
pjgpetecodes <pete@pjgcreations.co.uk>
2020-05-05 15:54:10 +0100

    Make Build Happen

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ setInterval(function(){
     if (err) {
       console.error('send error: ' + err.toString());
     } else {
-      console.log('message sent to the IoT Hub... Jenkins Here!');
+      console.log('message sent to the IoT Hub!');
     }
   });
 }, 1000);
